7f72700cd27971e693f7247547165d2d881e7f6b
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2023-08-15 14:55:55 +0200

    finish day5

--- day5/day5.go
+++ day5/day5.go
@@ -4,9 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"regexp"
+	"slices"
+	"strconv"
 	"strings"
 )
+
+func parseInstructions(line string) (quantity, from, to int) {
+	re := regexp.MustCompile(`\d+`)
+	splitted := re.FindAllString(line, -1)
+
+	quantity, _ = strconv.Atoi(splitted[0])
+	from, _ = strconv.Atoi(splitted[1])
+	to, _ = strconv.Atoi(splitted[2])
+
+	from--
+	to--
+
+	return
+}
 
 func main() {
 	file, err := os.Open("./file")
@@ -29,6 +47,7 @@ func main() {
 
 		if mode == 0 {
 			drawing = append(drawing, line)
+			fmt.Println(line)
 		} else {
 			instructions = append(instructions, line)
 		}
@@ -36,15 +55,51 @@ func main() {
 
 	lastEl, drawing := drawing[len(drawing)-1], drawing[:len(drawing)-1]
 	numCol := len(strings.Split(lastEl, " ")) / 3
-	fmt.Println(drawing)
-	fmt.Println(numCol)
 
-	// We have everything to start filling columns
-	// number of columns and drawing in a slice
-	// we still need to split each line
-	// we know it will come as a multiple of 3
-	// 2 solutions that comes to my mind
-	// we use something like split (strings or slices method) this should output [3]string
-	// or we loop by len(slice) / 3, this should give us column by column
-	// we need two loop i,j.
+	// Creating empty columns
+	var result [][]string
+	for i := 0; i < numCol; i++ {
+		result = append(result, make([]string, 0))
+	}
+
+	// filling columns
+	re := regexp.MustCompile(`\w`)
+	for i := 0; i < len(drawing); i++ {
+		for j := 0; j < len(drawing[i]); j++ {
+			if re.MatchString(string(drawing[i][j])) {
+				letter := string(drawing[i][j])
+				col := int(math.Ceil(float64(j / 4)))
+				result[col] = append(result[col], letter)
+			}
+		}
+	}
+
+	for _, col := range result {
+		slices.Reverse(col)
+	}
+
+	// reading instructions
+	for _, line := range instructions {
+		quantity, from, to := parseInstructions(line)
+		fmt.Println(result)
+
+		fromCol := result[from]
+		temp := make([]string, 0)
+
+		for i := 0; i < quantity; i++ {
+			elems, col := fromCol[len(fromCol)-1], fromCol[:len(fromCol)-1]
+			fromCol = col
+			temp = append(temp, elems)
+		}
+
+		result[from] = fromCol
+		slices.Reverse(temp)
+		result[to] = append(result[to], temp...)
+	}
+
+	var str []string
+	for _, line := range result {
+		str = append(str, line[len(line)-1])
+	}
+	fmt.Println(strings.Join(str, ""))
 }
